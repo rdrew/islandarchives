03a57de4a667d00ada9ddc1d47717b0c894c5734
Rob Drew <rdrew@upei.ca>
2016-12-13 08:27:43 -0400

    remove https in font import

--- nproxy_rules.js
+++ nproxy_rules.js
@@ -1,8 +1,8 @@
 module.exports = [
 	// 1. replace single file with local one
 	{
-		pattern: 'crap_im_not_proud_of.styles.css',      // Match url you wanna replace
-		responder:  "/var/www/html/drupal/sites/all/themes/islandarchives/css/crap_im_not_proud_of.styles.css"
+		pattern: 'islandarchives.styles.css',      // Match url you wanna replace
+		responder:  "/home/rdrew/themes/islandarchives/css/islandarchives.styles.css"
 	}
 
 
