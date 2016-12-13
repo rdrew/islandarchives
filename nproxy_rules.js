63b1d2fc5406838e4fc66d0027896dce63c91653
rdrew <robdrewupei@gmail.com>
2016-12-13 16:20:08 -0400

    .

--- nproxy_rules.js
+++ nproxy_rules.js
@@ -1,9 +1,17 @@
 module.exports = [
 	// 1. replace single file with local one
-	{
-		pattern: 'islandarchives.styles.css',      // Match url you wanna replace
-		responder:  "/home/rdrew/themes/islandarchives/css/islandarchives.styles.css"
-	}
+//{
+	//pattern: 'islandarchives.styles.css',      // Match url you wanna replace
+	//responder:  "/home/rdrew/themes/islandarchives/css/islandarchives.styles.css"
+//}
+{
+	pattern: 'sites/all/themes/islandarchives/css',  // must be a string
+	responder: '/Users/rdrew/themes/islandarchives/css/' //must be a absolute directory path
+},
+{
+	pattern: 'sites/all/themes/islandarchives/js',  // must be a string
+	responder: '/Users/rdrew/themes/islandarchives/js/' //must be a absolute directory path
+}
 
 
 ]
