42a748a7952a9c36d74bf66bc73f7e3320a2c247
Rob Drew <rdrew@upei.ca>
2016-12-20 14:47:42 -0400

    landing page js

--- js/islandarchives.behaviors.js
+++ js/islandarchives.behaviors.js
@@ -1,46 +1,65 @@
 (function($) {
 
-    Drupal.behaviors.stickyHeader = {
-        attach: function(context, settings) {
-            $(window).scroll(function() {
-                if ($(this).scrollTop() > 1) {
-                    $('header').addClass("sticky");
-                } else {
-                    $('header').removeClass("sticky");
-                }
+	//Drupal.behaviors.frontpage_tiles = {
+		//attach: function(context, settings) {
 
-            });
-        }
-    }
+      //$(".collection-slide").hide();
 
-    Drupal.behaviors.collection_view_buttons = {
-        attach: function(context, settings) {
-            $('.islandora-basic-collection-display-switch .islandora-view-list', context).once('foo', function() {
-                // Now, we are invoking the previously declared theme function using two
-                $(this).empty().append('<i class="fa fa-list" aria-hidden="true"></i>');
-            });
-            $('.islandora-basic-collection-display-switch .islandora-view-grid', context).once('foo', function() {
-                // Now, we are invoking the previously declared theme function using two
-                $(this).empty().append('<i class="fa fa-th" aria-hidden="true"></i>');
-            });
-            $('.pager__item--first', context).once('collection_view_buttons', function() {
-                // Now, we are invoking the previously declared theme function using two
-                $(this).prepend('<i class="fa fa-angle-double-left" aria-hidden="true"></i>');
-            });
-            $('.pager__item--previous', context).once('collection_view_buttons', function() {
-                // Now, we are invoking the previously declared theme function using two
-                $(this).prepend('<i class="fa fa-angle-left" aria-hidden="true"></i>');
-            });
-            $('.pager__item--next', context).once('collection_view_buttons', function() {
-                // Now, we are invoking the previously declared theme function using two
-                $(this).append('<i class="fa fa-angle-right" aria-hidden="true"></i>');
-            });
-            $('.pager__item--last', context).once('collection_view_buttons', function() {
-                // Now, we are invoking the previously declared theme function using two
-                $(this).append('<i class="fa fa-angle-double-right" aria-hidden="true"></i>');
-            });
-        }
-    }
+			//$(".collection-tile").click(function(event) {
+				//event.preventDefault();
+
+				//$(".collection-slide").hide();
+				//var activeSlide = $(this).attr("rel");
+				//$("#"+activeSlide).slideDown("slow");
+
+
+			//});
+
+
+		//}
+	//}
+
+	Drupal.behaviors.stickyHeader = {
+		attach: function(context, settings) {
+			$(window).scroll(function() {
+				if ($(this).scrollTop() > 1) {
+					$('header').addClass("sticky");
+				} else {
+					$('header').removeClass("sticky");
+				}
+
+			});
+		}
+	}
+
+	Drupal.behaviors.collection_view_buttons = {
+		attach: function(context, settings) {
+			$('.islandora-basic-collection-display-switch .islandora-view-list', context).once('foo', function() {
+				// Now, we are invoking the previously declared theme function using two
+				$(this).empty().append('<i class="fa fa-list" aria-hidden="true"></i>');
+			});
+			$('.islandora-basic-collection-display-switch .islandora-view-grid', context).once('foo', function() {
+				// Now, we are invoking the previously declared theme function using two
+				$(this).empty().append('<i class="fa fa-th" aria-hidden="true"></i>');
+			});
+			$('.pager__item--first', context).once('collection_view_buttons', function() {
+				// Now, we are invoking the previously declared theme function using two
+				$(this).prepend('<i class="fa fa-angle-double-left" aria-hidden="true"></i>');
+			});
+			$('.pager__item--previous', context).once('collection_view_buttons', function() {
+				// Now, we are invoking the previously declared theme function using two
+				$(this).prepend('<i class="fa fa-angle-left" aria-hidden="true"></i>');
+			});
+			$('.pager__item--next', context).once('collection_view_buttons', function() {
+				// Now, we are invoking the previously declared theme function using two
+				$(this).append('<i class="fa fa-angle-right" aria-hidden="true"></i>');
+			});
+			$('.pager__item--last', context).once('collection_view_buttons', function() {
+				// Now, we are invoking the previously declared theme function using two
+				$(this).append('<i class="fa fa-angle-double-right" aria-hidden="true"></i>');
+			});
+		}
+	}
 
 
 })(jQuery);
--- templates/page/page--front.tpl.php
+++ templates/page/page--front.tpl.php
@@ -27,6 +27,7 @@
 	  </div>
 	</header>
 
+	<?php print $messages; ?>
 	<div class="hero">
 	  <div class="hero-content">
 		<img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/placeholder_square.png" alt="Logo Image" class="hero-logo">
@@ -37,50 +38,56 @@
     </p>
 	  </div>
 	</div>
-	<div class="grid-items-lines">
-	  <a href="javascript:void(0)" class="grid-item">
+
+<!--collection grid tiles-->
+<!--
+	<div class="grid grid-items-lines">
+	  <a href="javascript:void(0)" rel="one" class="collection-tile grid-item">
 		<img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/placeholder_square.png" alt="">
 		<h1>Grid Item</h1>
 		<p>Lorem ipsum dolor sit amet, elit. Rem, illum.</p>
 	  </a>
-	  <a href="javascript:void(0)" class="grid-item">
+	  <a href="javascript:void(0)" rel="two" class="collection-tile grid-item">
 		<img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/placeholder_square.png" alt="">
 		<h1>Another Item</h1>
 		<p>Lorem ipsum consectetur dolor sit amet, consectetur adipisicing elit.</p>
 	  </a>
-	  <a href="javascript:void(0)" class="grid-item">
+	  <a href="javascript:void(0)" rel="three" class="collection-tile grid-item">
 		<img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/placeholder_square.png" alt="">
 		<h1>Another Grid Item</h1>
 		<p>Lorem ipsum dolor sit amet, elit.</p>
 	  </a>
-	  <a href="javascript:void(0)" class="grid-item">
+	  <a href="javascript:void(0)" rel="four" class="collection-tile grid-item">
 		<img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/placeholder_square.png" alt="">
 		<h1>Grid Item</h1>
 		<p>Lorem ipsum dolor sit amet, elit. Rem, illum.</p>
 	  </a>
-	  <a href="javascript:void(0)" class="grid-item grid-item-big">
+	  <a href="javascript:void(0)" rel="five" class="collection-tile grid-item">
 		<img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/placeholder_square.png" alt="">
 		<h1>Wider Grid Item</h1>
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem, illum.</p>
 	  </a>
-	  <a href="javascript:void(0)" class="grid-item">
+	  <a href="javascript:void(0)" rel="six" class="collection-tile grid-item">
 		<img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/placeholder_square.png" alt="">
 		<h1>A Grid Item</h1>
-		<p>Lorem ipsum dolor sit amet, elit. Rem, illum.</p>
-	  </a>
-	  <a href="javascript:void(0)" class="grid-item">
-		<img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/placeholder_square.png" alt="">
-		<h1>Item</h1>
-		<p>Lorem ipsum dolor sit amet, elit. Rem, illum.</p>
-	  </a>
-	  <a href="javascript:void(0)" class="grid-item">
-		<img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/placeholder_square.png" alt="">
-		<h1>Last Grid Item</h1>
 		<p>Lorem ipsum dolor sit amet, elit. Rem, illum.</p>
 	  </a>
 	  <div class="right-cover"></div>
 	  <div class="bottom-cover"></div>
 	</div>
+-->
+<!--end of the tiles-->
+<!--collection slides-->
+
+
+<!--begin views embed-->
+
+<?php print views_embed_view('landing_page_grid', 'collection_grid'); ?>
+<?php print views_embed_view('landing_page_grid', 'collection_slides'); ?>
+
+<!--end views embed-->
+
+
   <footer class="l-footer-wrapper" role="contentinfo">
 	<?php print render($page['footer']); ?>
   </footer>
