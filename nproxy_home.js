11b9b3e64f81c8c8bc7839e3c2e19ecf137b941c
Rob Drew <rdrew@upei.ca>
2017-01-09 18:40:00 -0400

    font changes

--- nproxy_home.js
+++ nproxy_home.js
@@ -1,17 +1,9 @@
 module.exports = [
-	// 1. replace single file with local one
-//{
-	//pattern: 'islandarchives.styles.css',      // Match url you wanna replace
-	//responder:  "/home/rdrew/themes/islandarchives/css/islandarchives.styles.css"
-//}
-{
-	pattern: 'sites/all/themes/islandarchives/css',  // must be a string
-	responder: '/home/rdrew/themes/islandarchives/css/' //must be a absolute directory path
-},
-{
-	pattern: 'sites/all/themes/islandarchives/js',  // must be a string
-	responder: '/home/rdrew/themes/islandarchives/js/' //must be a absolute directory path
-}
 
+{ pattern: 'sites/all/themes/islandarchives/css',
+	responder: '/home/rdrew/themes/islandarchives/css/'},
 
-]
+{ pattern: 'sites/all/themes/islandarchives/js',
+  responder: '/home/rdrew/themes/islandarchives/js/'}]
+
+// run the following command: "nproxy -l nproxy_home.js"
