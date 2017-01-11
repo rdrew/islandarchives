03c5fffdbf7eac1ec182a95868fd02f6a7cd29fe
Rob Drew <rdrew@upei.ca>
2017-01-11 16:20:09 -0400

    template tidy

--- templates/islandora/islandora-basic-collection-grid.tpl.php
+++ templates/islandora/islandora-basic-collection-grid.tpl.php
@@ -6,14 +6,16 @@
  *
  * @TODO: needs documentation about file and variables
  */
+dpm ( get_defined_vars() );
 ?>
 
 <div class="islandora islandora-basic-collection">
   <div class="islandora-basic-collection-grid clearfix flex-boxes">
   <?php foreach($associated_objects_array as $key => $value): ?>
     <dl class="flex-box islandora-basic-collection-object <?php print $value['class']; ?>">
-        <dt class="islandora-basic-collection-thumb"><?php print $value['thumb_link']; ?></dt>
-        <dd class="flex-title islandora-basic-collection-caption"><?php print filter_xss($value['title_link']); ?></dd>
+        <a href="/<?php print $value['path']; ?>">
+        <dt class="islandora-basic-collection-thumb"><?php print $value['thumbnail']; ?></dt>
+        <dd class="flex-title islandora-basic-collection-caption"><?php print filter_xss($value['title']); ?></dd></a>
     </dl>
   <?php endforeach; ?>
 </div>
