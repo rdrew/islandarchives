5d3a408d5073c9c612e5bb09d097b07ee11c07e0
Rob Drew <rdrew@upei.ca>
2017-02-22 16:23:24 -0400

    changed folder structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,72 +1,150 @@
-'use strict';
-var gulp, sass, jade, sourcemaps, autoprefixer, importer, plumber, browserSync, sass_config;
+var gulp = require("gulp"),
+  sass = require("gulp-sass"),
+  browserSync = require("browser-sync").create(),
+  runSequence = require("run-sequence"),
+  del = require("del"),
+  glob = require("glob"),
+  autoprefixer = require("gulp-autoprefixer"),
+  importer = require('node-sass-globbing');
 
-gulp = require('gulp');
-sass = require('gulp-sass');
-sourcemaps = require('gulp-sourcemaps');
-autoprefixer = require('gulp-autoprefixer');
-importer = require('node-sass-globbing');
-plumber = require('gulp-plumber');
-browserSync = require('browser-sync').create();
-jade = require('gulp-jade-drupal');
 
-sass_config = {
-    importer: importer,
-    includePaths: [
-        //'node_modules/breakpoint-sass/stylesheets/',
-        //'node_modules/singularitygs/stylesheets/',
-        //'node_modules/compass-mixins/lib/'
-        //'node_modules/bourbon/app/assets/stylesheets/',
-        //'node_modules/bourbon-neat/app/assets/stylesheets/'
+var config = {
+
+  // CHANGE THIS!
+  remoteURL: "http://hp1.islandarchives.ca/",
+  themeName: "islandarchives",
+  srcDir: "./src",
+  injectDir: "./build",
+  //localPath: "/local-assets",
+  localPath: "/sites/all/themes/islandarchives/build",
+
+  localAssets: {
+    css: [
+      "css/**/*.css"
+    ],
+    js: [
+      "js/**/*.js"
     ]
+  }
+
 };
 
-gulp.task('browser-sync', function() {
-    browserSync.init({
-        injectChanges: true,
-        //proxy: "localhost/drupal"
-      proxy: "http://hp1.islandarchives.ca",
-        //files: ["css/*.css"],
-    });
-    //gulp.watch("./sass/**/*.scss", ['sass:dev']).on('change', browserSync.reload);
-    gulp.watch("./src/sass/**/*.scss", ['sass:dev']);
-      gulp.watch("./build/css/**/*.css").on('change', browserSync.reload)
+var sass_config = {
+
+  importer: importer,
+
+  includePaths: [
+    //'node_modules/breakpoint-sass/stylesheets/',
+    //'node_modules/singularitygs/stylesheets/',
+    'node_modules/compass-mixins/lib/',
+    //'node_modules/bourbon/app/assets/stylesheets/',
+    //'node_modules/bourbon-neat/app/assets/stylesheets/'
+  ]
+
+
+};
+
+gulp.task("clean", function() {
+  return del.sync(config.injectDir);
 });
 
-gulp.task('templates', function() {
-  gulp.src('./templates/jade/**/*.jade')
-    .pipe(jade({
-        locals: {
-          title: 'OMG THIS IS THE TITLE'
+gulp.task("sass", function() {
+  return gulp.src(config.srcDir + "/sass/**/*.scss")
+    .pipe(sass(sass_config).on("error", sass.logError))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest(config.injectDir + "/css"))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('images', function() {
+  return gulp.src(config.srcDir + '/img/**/*')
+    .pipe(gulp.dest(config.injectDir + '/img'))
+});
+
+gulp.task("js", function() {
+  return gulp.src(config.srcDir + "/js/**/*.js")
+    .pipe(gulp.dest(config.injectDir + "/js"))
+    .pipe(browserSync.stream());
+});
+
+gulp.task("browserSync", ["sass", "js"], function() {
+  var targetCss = config.themeName;
+  browserSync.init({
+    proxy: {
+      target: config.remoteURL
+    },
+    rewriteRules: [
+      {
+        match: /css\/islandarchives/g,
+        //match: targetCss,
+        fn: function (req, res, match) {
+          return 'null';
         }
-     }))
-     .pipe(gulp.dest('./templates'));
+
+
+      },
+
+      {
+        // Inject Local CSS at the end of HEAD
+        match: /<\/head>/i,
+        fn: function(req, res, match) {
+          var localCssAssets = "";
+          for (var i = 0; i < config.localAssets.css.length; i++) {
+
+            var files = glob.sync(config.localAssets.css[i], {
+              cwd: config.injectDir
+            });
+
+            for (var file in files) {
+              localCssAssets += "<link rel=\"stylesheet\" type=\"text/css\" href=\"" + config.localPath + "/" + files[file] + "\">";
+              //localCssAssets += "<link rel=\"stylesheet\" type=\"text/css\" href=\"" + config.localPath + "/" + config.themeName + "/" + files[file] + "\">";
+            }
+          }
+
+          return localCssAssets + match;
+        }
+      }, {
+        // Inject Local JS at the end of BODY
+        match: /<\/body>/i,
+        fn: function(req, res, match) {
+          var localJsAssets = "";
+          for (var i = 0; i < config.localAssets.js.length; i++) {
+
+            var files = glob.sync(config.localAssets.js[i], {
+              cwd: config.injectDir
+            });
+
+            for (var file in files) {
+              localJsAssets += "<script src=\"" + config.localPath + "/" + files[file] + "\"></script>";
+            }
+          }
+
+          return localJsAssets + match;
+        }
+      }],
+    serveStatic: [{
+      route: config.localPath,
+      dir: config.injectDir
+    }],
+    watchTask: true
+  });
 });
 
-gulp.task('sass:prod', function() {
-    gulp.src('./src/sass/**/*.scss')
-        .pipe(sass(sass_config).on('error', sass.logError))
-        .pipe(autoprefixer({
-            browsers: ['last 2 version']
-        }))
-        .pipe(gulp.dest('./build/css'));
+gulp.task("watch", ["browserSync", "js", "images", "sass"], function() {
+  gulp.watch(config.srcDir + "/sass/**/*.scss", ["sass"]);
+  gulp.watch(config.srcDir + "/img/**/*", ["images"]);
+  gulp.watch(config.srcDir + "/js/**/*.js", ["js"]);
 });
 
-gulp.task('sass:dev', function() {
-    gulp.src('./src/sass/*.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass(sass_config).on('error', sass.logError))
-        .pipe(autoprefixer({
-            browsers: ['last 2 version']
-        }))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./build/css'));
+gulp.task("build", function() {
+  runSequence([
+    "clean",
+    "sass",
+    "js",
+    "images"
+  ]);
 });
 
-//gulp.task('sass:watch', function () {
-//gulp.watch('./sass/**/*.scss', ['sass:dev']);
-//});
-
-//gulp.task('default', ['sass:dev', 'sass:watch']);
-gulp.task('default', ['browser-sync']);
-gulp.task('live', ['sass:prod']);
+gulp.task("default", function() {
+  runSequence(["build", "browserSync", "watch"]);
+});
